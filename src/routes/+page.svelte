9d1e5f1f3b6ec2dea707a9b090b3e12a771203d6
Blaeriz <pinglehrishikesh@yahoo.in>
2025-04-19 03:01:55 +0530

    made the navbar

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- NAVBAR -->
+<div class="flex w-full flex-row items-center border-b border-gray-700">
+	<div
+		class="max-w-[1200px] w-full flex flex-row items-center justify-between align-middle mx-auto"
+	>
+		<p class="text-5xl font-extrabold uppercase font-vt323">Hppingle</p>
+		<div class="flex flex-row space-x-4 text-2xl">
+			<a href="/" class="uppercase hover:underline">Home</a>
+			<a href="/" class="uppercase hover:underline">About</a>
+		</div>
+	</div>
+</div>
+<div class="mx-auto flex w-full max-w-[1200px] flex-col p-4 font-vt323">
+	<!-- DIVIDER -->
+</div>
